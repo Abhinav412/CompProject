1aef829060e280324722b415c2b61344f646beb2
Abhinav <abhinavdasu0412@gmail.com>
2023-01-04 11:58:14 +0530

    Update Project tkinter GUI.py

--- Project tkinter GUI.py
+++ Project tkinter GUI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 root = Tk()
 root.title('ADHD BANK')
+root.config(bg='red')
 T = Label(root, text='ADHD BANK', font=("Bold",150),bg='red', fg='blue')
 T.place(relx=0.5,rely=0.5,anchor='center')
 T.pack()
